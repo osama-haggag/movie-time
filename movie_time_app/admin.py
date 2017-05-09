f2bf96af030718d5af60ea3a3a8c9a027f7da5b2
Osama <osama.haggag@digitalperformance.de>
2017-05-09 15:20:05 +0200

    Add models and register them with admin

--- movie_time_app/admin.py
+++ movie_time_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from movie_time_app.models import Movie, Similarity, Tag
+
+admin.site.register(Movie)
+admin.site.register(Similarity)
+admin.site.register(Tag)
--- movie_time_app/models.py
+++ movie_time_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    movie_id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=200)
+    poster = models.ImageField(null=True, blank=True)
+    year = models.IntegerField()
+    genres = models.CharField(max_length=200)
+    rating_median = models.FloatField()
+    rating_mean = models.FloatField()
+
+
+class Similarity(models.Model):
+    first_movie = models.ForeignKey(Movie, related_name='first_movie')
+    second_movie = models.ForeignKey(Movie, related_name='second_movie')
+    similary_score = models.FloatField()
+
+
+class Tag(models.Model):
+    movie = models.ForeignKey(Movie)
+    tag = models.CharField(max_length=50)
+    relevance = models.FloatField()
