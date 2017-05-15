0f17cf301826f16bc23e318fcae18492c660450c
Osama <osama.haggag@digitalperformance.de>
2017-05-15 16:01:31 +0200

    HTML template for showing lists of items

--- movie_time_app/views.py
+++ movie_time_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'sent_things': ['a', 'test', 'list']
+    }
+    return render(request, 'index.html', context=context)
