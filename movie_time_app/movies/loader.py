57788c2a0138fb1fc4e7d0c351b63ce9cb5972b2
Osama <osama.haggag@digitalperformance.de>
2017-05-17 15:24:17 +0200

    Add preliminary code for selecting recommendations

--- movie_time_app/movies/loader.py
+++ movie_time_app/movies/loader.py
@@ -1,5 +1,25 @@
-from movie_time_app.models import Movie
+from movie_time_app.models import Movie, LikedOrNot, Similarity
 
+
+TOP_N = 10
 
 def load_unrelatable_movies(n):
     return Movie.objects.filter(relatable=False).order_by('?')[:n]
+
+
+def _get_similar_movies(movies):
+    top_movies = []
+    for movie in movies:
+        similar_movies = Similarity.objects.filter(
+            first_movie=movie.movie_id,
+            second_movie__movie_id__in).order_by('similarity_score')[:TOP_N]
+
+        top_movies.append(Movie.objects.filter())
+
+
+def load_similar_to_liked_movies(n):
+    liked_movies = LikedOrNot.objects.all().order_by('?')
+    similar_movies = _get_similar_movies(liked_movies)
+
+
+    movies = Movie.objects.filter(relatable=True).order_by('')
