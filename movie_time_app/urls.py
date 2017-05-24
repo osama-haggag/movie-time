031cdaf2e286d712f930242632eee735df6ee558
Osama Haggag <osama.haggag@gmail.com>
2017-05-24 19:28:25 +0200

    Add recommendations to homepage

--- movie_time_app/urls.py
+++ movie_time_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<movie_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^search/$', views.search, name='search')
+    url(r'^search/$', views.search, name='search'),
+    url(r'^update/(?P<movie_id>[0-9]+)/$', views.update, name='update')
 ]
--- movie_time_app/views.py
+++ movie_time_app/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
+from movie_time_app.models import Movie
 from movie_time_app.movies.detail import load_movie_detail
-from movie_time_app.movies.loader import load_unrelatable_movies, load_recommendations
+from movie_time_app.movies.homepage import load_homepage_recommendations
 from movie_time_app.movies.search import search_for_query
 
 INDEX_TEMPLATE_NAME = 'index.html'
@@ -17,8 +19,25 @@ def search(request):
     search_results, template_name = search_for_query(request.GET)
     return render(request, template_name, context=search_results)
 
+def update(request, movie_id):
+    movie = Movie.objects.get(pk=movie_id)
+    if 'liked' in request.POST:
+        movie.liked_or_not = True
+    elif 'disliked' in request.POST:
+        movie.liked_or_not = False
+    elif 'reset' in request.POST:
+        movie.liked_or_not = None
+
+    movie.save()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
 
 def index(request):
-    recommendations = load_recommendations()
-    response = render(request, 'index.html', context={})
+    liked, not_liked, random = load_homepage_recommendations()
+    context = {
+        'liked': liked,
+        'not_liked': not_liked,
+        'random': random
+    }
+    response = render(request, 'index.html', context=context)
     return response
